1b8f23e3066b07d9d94ceabc5e37ce1b2c64d8e8
faust93 <monumentum@gmail.com>
2018-09-19 14:26:54 +0800

    RIL: Fix manual network selection with old modem
     * Old modem sends and applies mccmnc+network type, but new aosp
       telephony uses only digits.
     * Manually cut or append the "+" and strings after that to work around.
    
    Change-Id: I1015e6c0d6afdc0c03d70e9491d49331703eddcc
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -222,6 +222,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
     final Integer mPhoneId;
 
+    private boolean mUseOldMncMccFormat;
+
     private static final String PROPERTY_IS_VONR_ENABLED = "persist.radio.is_vonr_enabled_";
 
     public static final int MIN_SERVICE_IDX = HAL_SERVICE_RADIO;
@@ -1153,6 +1155,9 @@ public class RIL extends BaseCommands implements CommandsInterface {
              * SecurityException and return correct value based on what HAL we're testing. */
             if (proxies == null) throw ex;
         }
+
+        mUseOldMncMccFormat = SystemProperties.getBoolean(
+                "ro.telephony.use_old_mnc_mcc_format", false);
 
         TelephonyManager tm = (TelephonyManager) context.getSystemService(
                 Context.TELEPHONY_SERVICE);
@@ -2572,14 +2577,20 @@ public class RIL extends BaseCommands implements CommandsInterface {
         RILRequest rr = obtainRequest(RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL, result,
                 mRILDefaultWorkSource);
 
+        if (mUseOldMncMccFormat && !TextUtils.isEmpty(operatorNumeric)) {
+            operatorNumeric += "+";
+        }
+
         if (RILJ_LOGD) {
             riljLog(rr.serialString() + "> " + RILUtils.requestToString(rr.mRequest)
                     + " operatorNumeric = " + operatorNumeric + ", ran = " + ran);
         }
 
+        final String operatorNumericCopy = operatorNumeric;
+
         radioServiceInvokeHelper(HAL_SERVICE_NETWORK, rr, "setNetworkSelectionModeManual", () -> {
             networkProxy.setNetworkSelectionModeManual(rr.mSerial,
-                    RILUtils.convertNullToEmptyString(operatorNumeric), ran);
+                    RILUtils.convertNullToEmptyString(operatorNumericCopy), ran);
         });
     }
 
